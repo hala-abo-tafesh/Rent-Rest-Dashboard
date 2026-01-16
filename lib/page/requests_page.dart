e46c60c66fbe916b4365760de719d22202ba7a8b
unknown <hhla20920@gmail.com>
2026-01-16 20:43:03 +0300

    a

--- lib/page/requests_page.dart
+++ lib/page/requests_page.dart
@@ -4,46 +4,55 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widgets/request_card.dart';
 import '../theme/colors.dart';
 
-class RequestsPage extends StatelessWidget {
+class RequestsPage extends StatefulWidget {
   const RequestsPage({super.key});
+
+  @override
+  State<RequestsPage> createState() => _RequestsPageState();
+}
+
+class _RequestsPageState extends State<RequestsPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<UsersCubit>().getUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Requests'),
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        foregroundColor: Theme.of(context).brightness == Brightness.dark
-            ? AppColors.textPrimaryDark
-            : AppColors.textPrimaryLight,
-        elevation: 0,
         centerTitle: true,
       ),
       body: BlocBuilder<UsersCubit, UsersState>(
         builder: (context, state) {
           final cubit = context.read<UsersCubit>();
 
+          if (state is GetUsersLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
           final pending = cubit.usersModel
               .where((u) => u.status.trim().toLowerCase() == 'pending')
               .toList();
 
-          if(state is GetUsersLoadingState) {
-            return Center(child: CircularProgressIndicator(color: AppColors.primary,));
+          if (pending.isEmpty) {
+            return const Center(child: Text('No pending requests'));
           }
 
           return ListView.separated(
-            padding: const EdgeInsets.symmetric(vertical: 8),
             itemCount: pending.length,
             itemBuilder: (context, index) {
               final user = pending[index];
-
               return RequestCard(
                 user: user,
-                onAccept: () => cubit.acceptUser(userID : user.id),
-                onReject: () => cubit.rejectUser(userID : user.id),
+                onAccept: () => cubit.acceptUser(userID: user.id),
+                onReject: () => cubit.rejectUser(userID: user.id),
               );
             },
-            separatorBuilder: (context, index) => const SizedBox(height: 0),
+            separatorBuilder: (_, __) => const SizedBox(height: 8),
           );
         },
       ),
